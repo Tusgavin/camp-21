bdb5b0d879d4118ca1cc6892955b6d640ae5f94c
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-04-15 20:36:24 -0300

    feature(adminRoutes): admin routes must be authorized and accessed by admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { adminController } = require("../controllers");
+const { isAuthAdministrator } = require("../middlewares");
 
-// CERTIFICAR QUE QUEM ESTÀ FAZENDO CHAMADA È ADMINISRADOR
+router.use(isAuthAdministrator);
 
 router.patch("/make-admin", adminController.makeAdmin);
 router.post("/make-admin", adminController.createAdmin)
