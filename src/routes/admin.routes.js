321e5145d600ccb27af8c36b75623dcb83f27245
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-04-15 22:15:54 -0300

    feature(adminRoutes):  edit a normal user to become admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,6 +5,10 @@ const { isAuthAdministrator } = require("../middlewares");
 router.use(isAuthAdministrator);
 
 router.patch("/make-admin", adminController.makeAdmin);
-router.post("/make-admin", adminController.createAdmin)
+router.post("/make-admin", adminController.createAdmin);
+
+router.post("/create-film", adminController.createFilm);
+router.patch("/edit-film", adminController.editFilm);
+router.delete("/delete-film", adminController.deleteFilm);
 
 module.exports.admin = router;
--- src/services/admin/makeAdmin.service.js
+++ src/services/admin/makeAdmin.service.js
@@ -3,5 +3,24 @@ const { usersRepository } = require("../../repositories");
 const { messages } = require("../../helpers");
 
 module.exports.makeAdmin = async (id) => {
+   if (id === undefined) {
+      throw {
+         status: StatusCodes.UNPROCESSABLE_ENTITY,
+         message: messages.missingParams,
+      }
+   }
 
+   const user = await usersRepository.getById(id);
+
+   if (!user) {
+      throw {
+         status: StatusCodes.NOT_FOUND,
+         message: messages.notFound("user")
+      };
+   }
+
+   await usersRepository.setFields(user, { isAdmin: true });
+   const updatedUser = await usersRepository.update(user);
+
+   return { updatedUser }
 };
